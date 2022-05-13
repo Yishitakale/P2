7b3ac62b3547b2ed5f62823d32d2e277271cba45
Yishitakale <yishitakale@gmail.com>
2022-05-13 06:59:33 -0700

    first commit

---
+++ src/p2/Defaultdemo2.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class Defaultdemo2 {
+	static int b=90;
+	void display() {
+		System.out.println("Hello");
+}
+	public static void main(String[] args){
+		Defaultdemo2 D=new Defaultdemo2();
+		D.display();
+		System.out.println(b);
+	}
+
+}
